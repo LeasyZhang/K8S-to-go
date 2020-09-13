e16371deb77248e462848774bf6262ca041d20e3
joe zhang <joe.zhang@ringcentral.com>
2020-09-13 16:02:17 +0800

    add stateful set

--- project-demo/spring-boot-microservice/src/main/java/com/example/demo/controller/ShowIpController.java
+++ project-demo/spring-boot-microservice/src/main/java/com/example/demo/controller/ShowIpController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @RestController
 public class ShowIpController {
 
@@ -14,7 +16,7 @@ public class ShowIpController {
         String ipAddr = request.getRemoteAddr();
         String host = request.getRemoteHost();
         StringBuilder builder = new StringBuilder().append(host).append(":").append(ipAddr);
-
+        log.info("Access location api from {}", builder.toString());
         return ResponseEntity.ok(builder.toString());
     }
 }
